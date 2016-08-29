b38321d965b2ec6c0282cd6156f1dba7d4d0cb29
Sim Sang Chul <sangchul.sim@wemakeprice.com>
2016-08-29 23:50:50 +0900

    chatting창 angularjs 작업중

--- static/js/chat/application.js
+++ static/js/chat/application.js
@@ -1,56 +1,77 @@
 // https://github.com/joewalnes/reconnecting-websocket/
-if ("WebSocket" in window) {
-    var host = location.protocol.replace("http", "ws") + "//" + location.host + "/chat/ws";
-    var box = new ReconnectingWebSocket(host);
+if (!"WebSocket" in window) {
+    alert("WebSocket NOT supported by your Browser!");
+}
 
-    function systemMessage() {
-        var message = {
-            handle: "system.message",
-            text: "이것은 클라이언트에서 보내는 메시지 입니다."
+var host = location.protocol.replace("http", "ws") + "//" + location.host + "/chat/ws";
+var box = new ReconnectingWebSocket(host);
+
+function appendMessage(handle, message) {
+    var html = '<div>';
+    html += '<dl class="dl-horizontal">';
+    html += '<dt><span class="glyphicon glyphicon-user" aria-hidden="true"></span><span>' + handle + '</span></dt>';
+    html += '<dd><span class="label label-info">' + message + '</span></dd>';
+    html += '</dl>';
+    html += '</div>'
+    $("#chat-text").append(html);
+
+    $("#chat-text").stop().animate({
+        scrollTop: $('#chat-text')[0].scrollHeight
+    }, 800);
+}
+
+// https://developer.mozilla.org/ko/docs/WebSockets/Writing_WebSocket_client_applications 참고
+// 서버로부터 데이터 수신
+box.onmessage = function(message) {
+    var data = JSON.parse(message.data);
+    console.log("box.onmessage")
+    console.dir(data);
+
+    if (data.hasOwnProperty("handle")) {
+        if (data.handle == "system.message" || data.handle == "room.message") {
+            appendMessage(data.user, data.text);
         }
-        box.send(JSON.stringify(message));
     }
+};
 
-    function appendMessage(handle, message) {
-        var html = '<div>';
-        html += '<dl class="dl-horizontal">';
-        html += '<dt><span class="glyphicon glyphicon-user" aria-hidden="true"></span><span>' + handle + '</span></dt>';
-        html += '<dd><span class="label label-info">' + message + '</span></dd>';
-        html += '</dl>';
-        html += '</div>'
-        $("#chat-text").append(html);
+// 연결 종료
+box.onclose = function() {
+    console.log('box closed');
+    this.box = new WebSocket(box.url);
+};
 
-        $("#chat-text").stop().animate({
-            scrollTop: $('#chat-text')[0].scrollHeight
-        }, 800);
-    }
+var app = angular.module("app", [
+    // "ngAnimate"
+    "ui.bootstrap"
+]);
 
-    // https://developer.mozilla.org/ko/docs/WebSockets/Writing_WebSocket_client_applications 참고
-    // 서버로부터 데이터 수신
-    box.onmessage = function(message) {
-        var data = JSON.parse(message.data);
-        console.log("box.onmessage")
-        console.dir(data);
+app.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist(["self", "**"]);
+});
 
-        if (data.hasOwnProperty("handle")) {
-            if (data.handle == "system.message" || data.handle == "room.message") {
-                appendMessage(data.handle, data.text);
-            }
-        }
-    };
+app.controller("ChatController", function($scope, $rootScope, $log, $http, $timeout) {
+    $scope.users = [
+        "1번유저",
+        "2번유저",
+        "3번유저",
+        "4번유저",
+        "5번유저",
+        "6번유저"
+    ];
+    $scope.rooms = [
+        "room1",
+        "room2",
+        "room3",
+        "room4",
+        "room5",
+    ];
 
-    // 연결 종료
-    box.onclose = function() {
-        console.log('box closed');
-        this.box = new WebSocket(box.url);
-    };
-
-    $("#input-form").on("submit", function(event) {
-        event.preventDefault();
-        var handle = $("#input-handle")[0].value;
-        var user = $("#input-user")[0].value;
-        var room = $("#input-room")[0].value;
-        var text = $("#input-text")[0].value;
+    $scope.sendMessage = function($event) {
+        $event.preventDefault();
+        var handle = "room.message";
+        var user = $scope.selectedUser;
+        var room = $scope.selectedRoom;
+        var text = $scope.message;
         if (handle == "" ||
             user == "" ||
             room == "" ||
@@ -70,8 +91,21 @@ if ("WebSocket" in window) {
         console.dir(message);
 
         box.send(JSON.stringify(message));
-        $("#input-text")[0].value = "";
-    });
-} else {
-    alert("WebSocket NOT supported by your Browser!");
-}
+        $scope.message = ""
+        $("#input-message").focus();
+    };
+
+    $scope.systemMessage = function($event) {
+        var message = {
+            handle: "system.message",
+            user: "system",
+            text: "이것은 system 메시지 입니다."
+        };
+        console.log("on.submit")
+        console.dir(message);
+
+        box.send(JSON.stringify(message));
+        $scope.message = ""
+        $("#input-message").focus();
+    }
+});
